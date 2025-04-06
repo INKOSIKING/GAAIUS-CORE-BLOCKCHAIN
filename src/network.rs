0200de1e8c4bd6c1988c084874551451568d3ec9
INKOSIKING <comfeezy@gmail.com>
2025-04-06 02:24:48 +0200

    Update network.rs

--- src/network.rs
+++ src/network.rs
@@ -1,62 +1,99 @@
+// src/network.rs
+
 use std::collections::HashSet;
-use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-use crate::blockchain::{Block, Blockchain};
+use crate::blockchain::Blockchain;
 use crate::transactions::Transaction;
-use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum NetworkMessage {
-    NewTransaction(Transaction),
-    NewBlock(Block),
-    RequestChain,
-    ResponseChain(Vec<Block>),
-    RegisterPeer(String),
+#[derive(Debug, Clone)]
+pub struct Peer {
+    pub address: String,
 }
 
-pub struct Node {
+impl Peer {
+    pub fn new(address: String) -> Self {
+        Peer { address }
+    }
+}
+
+pub struct P2PNetwork {
     pub peers: Arc<Mutex<HashSet<String>>>,
     pub blockchain: Arc<Mutex<Blockchain>>,
 }
 
-impl Node {
+impl P2PNetwork {
     pub fn new(blockchain: Arc<Mutex<Blockchain>>) -> Self {
-        Node {
+        P2PNetwork {
             peers: Arc::new(Mutex::new(HashSet::new())),
             blockchain,
         }
     }
 
-    pub fn start(&self, address: &str) {
-        let listener = TcpListener::bind(address).expect("Failed to bind to address");
-        println!("Node listening on {}", address);
+    pub fn start_listener(&self, port: u16) {
+        let listener = TcpListener::bind(("0.0.0.0", port)).expect("Failed to bind port");
+        println!("Listening for peers on port {}", port);
 
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    let peers = Arc::clone(&self.peers);
-                    let chain = Arc::clone(&self.blockchain);
+                    let peers = self.peers.clone();
+                    let blockchain = self.blockchain.clone();
                     thread::spawn(move || {
-                        Node::handle_connection(stream, peers, chain);
+                        P2PNetwork::handle_peer(stream, peers, blockchain);
                     });
                 }
                 Err(e) => {
-                    eprintln!("Connection error: {}", e);
+                    eprintln!("Failed to accept peer connection: {}", e);
                 }
             }
         }
     }
 
     pub fn connect_to_peer(&self, address: &str) {
-        if self.peers.lock().unwrap().insert(address.to_string()) {
-            let msg = serde_json::to_string(&NetworkMessage::RegisterPeer(address.to_string())).unwrap();
-            Node::send_message(address, &msg);
+        match TcpStream::connect(address) {
+            Ok(mut stream) => {
+                println!("Connected to peer at {}", address);
+                let mut peers = self.peers.lock().unwrap();
+                peers.insert(address.to_string());
+
+                let message = b"Hello GAAIUS Peer!";
+                stream.write_all(message).expect("Failed to write to peer");
+            }
+            Err(e) => {
+                eprintln!("Failed to connect to peer {}: {}", address, e);
+            }
         }
     }
 
-    pub fn broadcast(&self, msg: &NetworkMessage) {
-        let peers = self.peers.lock().unwrap().clone();
-        let serialized
+    fn handle_peer(mut stream: TcpStream, peers: Arc<Mutex<HashSet<String>>>, blockchain: Arc<Mutex<Blockchain>>) {
+        let mut buffer = [0; 512];
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                let data = String::from_utf8_lossy(&buffer[..size]);
+                println!("Received from peer: {}", data);
+
+                // Future: Handle syncing blocks or mempool transactions here.
+            }
+            Err(e) => {
+                eprintln!("Failed to read from peer: {}", e);
+            }
+        }
+
+        let peer_address = stream.peer_addr().unwrap().to_string();
+        peers.lock().unwrap().insert(peer_address);
+    }
+
+    pub fn broadcast_transaction(&self, tx: &Transaction) {
+        let peers = self.peers.lock().unwrap();
+        for peer in peers.iter() {
+            if let Ok(mut stream) = TcpStream::connect(peer) {
+                let msg = serde_json::to_string(tx).unwrap();
+                let _ = stream.write_all(msg.as_bytes());
+            }
+        }
+    }
+}
